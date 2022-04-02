99d337ca4044e0a0fbce9b97e2930fa6e6139213
nikhilmuneshwar0 <96412735+nikhilmuneshwar0@users.noreply.github.com>
2022-04-03 01:36:35 +0300

    Add files via upload

---
+++ TreasureIsland.py
@@ -0,0 +1,40 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+directionOne = input("You are at the cross road, go left or right.")
+if directionOne == 'left':
+    lakeInput = input("You are at a lake, swim or wait for a boat.")
+    if lakeInput == 'wait':
+        doorInput = input("There are three doors one is colored red, one is yellow and the other one is blue. Choose wisely.")
+        if doorInput == 'red':
+            print("The red room was buring and you fell into the fire and died.")
+        elif doorInput == "yellow":
+            print("You found the hidden tresure, You win.")
+        else:
+            print("Game over, You lose.")        
+    else:
+        print("You were attacked by alligators. You died.")    
+else:
+    print("The right road leads to a hole, and you fell in it.")
